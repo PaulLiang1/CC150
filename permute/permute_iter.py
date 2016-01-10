934f9d3e032b054a91f172be3c1fdb38842ae72b
PaulLiang1 <pliang@datalicious.com>
2016-01-10 21:36:35 +1100

    add permute iter solution

---
+++ permute/permute_iter.py
@@ -0,0 +1,32 @@
+from collections import deque
+
+class Solution:
+
+    def permute(self, nums):
+        # boundary check
+        if nums is None:
+            return list()
+        perms = list()
+
+        queue = deque()
+        # insert base case
+        queue.append((list(), 0))
+
+        while len(queue) > 0:
+            perm_str, index = queue.popleft()
+
+            if len(perm_str) == len(nums):
+                perms.append(perm_str)
+
+            else:
+                for pos in xrange(len(perm_str) + 1):
+                    new_str = perm_str[:]
+                    new_str.insert(pos, nums[index])
+                    queue.append((new_str, index + 1))
+
+        return perms
+
+if __name__ == '__main__':
+    s = Solution()
+
+    print s.permute([1, 2, 3])
